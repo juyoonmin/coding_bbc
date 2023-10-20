f6935a235857356384fd0f042b8990d9464b1619
juyoonmin <goaxhfl0109@naver.com>
2023-10-20 15:42:20 +0900

    bird애니메이션 추가

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 (() => {
+
+    const actions = {
+        birdFlies (key){
+            if(key){
+                document.querySelector('[data-index="3"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+            } else{
+                document.querySelector('[data-index="3"] .bird').style.transform = `translateX(-100%)`;
+            }
+        }
+    };
+
     const stepElems = document.querySelectorAll('.step')
     const graphicElems = document.querySelectorAll('.graphic-item')
     // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
@@ -19,12 +30,18 @@
         graphicElems[i].dataset.index = i;
     }
 
-    function activate() {
-        currentItem.classList.add('visible')
+    function activate(action) {
+        currentItem.classList.add('visible');
+        if(action){
+            actions[action](true);
+        }
     }
 
-    function inactivate() {
+    function inactivate(action) {
         currentItem.classList.remove('visible');
+        if(action){
+            actions[action](false);
+        }
     }
 
 
@@ -46,11 +63,15 @@
                 inactivate();
                 // 현재 보이는 step에 해당하는 currentItem을 graphic-item으로 설정
                 currentItem = graphicElems[step.dataset.index];
-                activate();
+                activate(currentItem.dataset.action);
 
             }
         }
     })
+
+    window.addEventListener('load', () =>{
+        setTimeout(()=> scrollTo(0, 0),100);
+    });
 
     activate();
 
